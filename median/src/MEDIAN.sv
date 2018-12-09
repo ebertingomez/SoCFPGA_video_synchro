5b3afae3a9107a2c8a51c4ae5d20e59a497bb2a8
Enrique <enrique.gomez@telecom-paristech.fr>
2018-12-09 13:33:29 +0100

    Corrections of the module MEDIAN. The indexes i, j and the loop where corrected

--- median/src/MEDIAN.sv
+++ median/src/MEDIAN.sv
@@ -9,20 +9,21 @@ module MEDIAN #(parameter W = 8)(
 
     enum logic[2:0] { INIT, STORE, SORT, DELETE, MEDIAN } state, n_state;
 
-    logic BYP, outp;
-    logic [3:0]i=5;
-    logic [4:0] j=9;
+    logic BYP;
+    logic [3:0]i=4;
+    logic [4:0] j=8;
 
     MED #(.W(W), .P(9))inst0 (.DI(DI), .DSI(DSI), .BYP(BYP), .CLK(CLK), .DO(DO));
 
-    assign DSO = (1==1)?outp:0;
+    assign DSO = (i==0 && j==0)?1:0;
     assign BYP = (j==0)? 1 : DSI;
 
     always_ff @(posedge CLK or posedge nRST)
         if (!nRST)
             begin
                 state <= INIT ;
-                outp <= 0;
+                i <= 4;
+                j <= 8;
             end
         else
             begin
@@ -35,7 +36,7 @@ module MEDIAN #(parameter W = 8)(
                         end
                     STORE : if (DSI == 0 && BYP == 0)
                         state <= SORT;
-                    SORT : if (BYP == 1)
+                    SORT : if (j==1)
                             begin
                                 if (i==0)
                                     state <= MEDIAN;
@@ -48,30 +49,19 @@ module MEDIAN #(parameter W = 8)(
                         state <= INIT;
                 endcase
                 // Les sorties
-                if (state == INIT || state == STORE)  
-                    outp <= 0;
-                else if (state == SORT) 
+                if (state == SORT) 
                     begin
-                        if ( i > 0) begin
-                            if ( j > 0 ) begin
-                                j <= j - i;
-                            end else begin
-                                i <= i - 1;
-                                j <= 9 - (5 - i);
-                            end
+                        if ( j > 0 ) begin
+                            j <= j - 1;
                         end else begin
-                            outp <= 1;
+                            i <= i - 1;
+                            j <= 9 - (5 - i + 1);
                         end
-                    end
-                else if (state == DELETE) 
-                    begin
-                        outp <= 0;
                     end
                 else if (state == MEDIAN) 
                     begin
-                        i <= 5;
-                        j <= 9;
-                        outp <= 0;
+                        i <= 4;
+                        j <= 8;
                     end
             end
 endmodule
