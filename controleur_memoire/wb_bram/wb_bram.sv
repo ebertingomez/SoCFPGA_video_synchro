5873a0e95797b69d59218967b8857e5ce136e3f3
Enrique <enrique.gomez@telecom-paristech.fr>
2018-12-11 17:29:15 +0100

    Incrementing burst address handling

--- controleur_memoire/wb_bram/wb_bram.sv
+++ controleur_memoire/wb_bram/wb_bram.sv
@@ -12,7 +12,9 @@ module wb_bram #(parameter mem_adr_width = 11) (
       // a vous de jouer a partir d'ici
       logic [3:0][7:0] memory [0: 2**mem_adr_width - 1];
       logic ack_read;
-      logic [3:0][7:0] masked_data;
+      logic [31:0] address;
+
+      assign address = (wb_s.cti == 2) ?  address : wb_s.adr[mem_adr_width+1:2] ;
 
       assign wb_s.ack = (wb_s.stb && wb_s.we) | ack_read;
 
@@ -20,29 +22,33 @@ module wb_bram #(parameter mem_adr_width = 11) (
       begin
             if ( wb_s.stb && wb_s.we )
                   case(wb_s.sel)
-                        4'b0001: memory[wb_s.adr[mem_adr_width+1:2]][0] <= wb_s.dat_ms[7:0];
-                        4'b0010: memory[wb_s.adr[mem_adr_width+1:2]][1] <= wb_s.dat_ms[15:8];
-                        4'b0100: memory[wb_s.adr[mem_adr_width+1:2]][2] <= wb_s.dat_ms[23:16];
-                        4'b1000: memory[wb_s.adr[mem_adr_width+1:2]][3] <= wb_s.dat_ms[31:24];
-                        4'b0011: memory[wb_s.adr[mem_adr_width+1:2]][1:0] <= wb_s.dat_ms[15:0];
-                        4'b1100: memory[wb_s.adr[mem_adr_width+1:2]][3:2] <= wb_s.dat_ms[31:16];
-                        4'b1111: memory[wb_s.adr[mem_adr_width+1:2]] <= wb_s.dat_ms;
+                        4'b0001: memory[address][0] <= wb_s.dat_ms[7:0];
+                        4'b0010: memory[address][1] <= wb_s.dat_ms[15:8];
+                        4'b0100: memory[address][2] <= wb_s.dat_ms[23:16];
+                        4'b1000: memory[address][3] <= wb_s.dat_ms[31:24];
+                        4'b0011: memory[address][1:0] <= wb_s.dat_ms[15:0];
+                        4'b1100: memory[address][3:2] <= wb_s.dat_ms[31:16];
+                        4'b1111: memory[address] <= wb_s.dat_ms;
                   endcase
             else if (wb_s.stb && ~wb_s.we && ~wb_s.ack)
                   case(wb_s.sel)
-                        4'b0001: wb_s.dat_sm <= {24'b0,memory[wb_s.adr[mem_adr_width+1:2]][0]};
-                        4'b0010: wb_s.dat_sm <= {16'b0,memory[wb_s.adr[mem_adr_width+1:2]][1],8'b0};
-                        4'b0100: wb_s.dat_sm <= {8'b0,memory[wb_s.adr[mem_adr_width+1:2]][2],16'b0};
-                        4'b1000: wb_s.dat_sm <= {memory[wb_s.adr[mem_adr_width+1:2]][3],24'b0};
-                        4'b0011: wb_s.dat_sm <= {16'b0,memory[wb_s.adr[mem_adr_width+1:2]][1:0]};
-                        4'b1100: wb_s.dat_sm <= {memory[wb_s.adr[mem_adr_width+1:2]][3:2],16'b0};
-                        4'b1111: wb_s.dat_sm <= {memory[wb_s.adr[mem_adr_width+1:2]]};
+                        4'b0001: wb_s.dat_sm <= {24'b0,memory[address][0]};
+                        4'b0010: wb_s.dat_sm <= {16'b0,memory[address][1],8'b0};
+                        4'b0100: wb_s.dat_sm <= {8'b0,memory[address][2],16'b0};
+                        4'b1000: wb_s.dat_sm <= {memory[address][3],24'b0};
+                        4'b0011: wb_s.dat_sm <= {16'b0,memory[address][1:0]};
+                        4'b1100: wb_s.dat_sm <= {memory[address][3:2],16'b0};
+                        4'b1111: wb_s.dat_sm <= {memory[address]};
                   endcase
 
             if ( wb_s.cti != 0 && wb_s.stb && ~wb_s.we)
                   case(wb_s.cti)
                         3'b001: ack_read <= 1'b1;
-                        3'b010: ack_read <= 1'b1;
+                        3'b010: begin
+                              ack_read <= 1'b1;
+                              address <= address + 1;
+                        end 
+
                         3'b111: ack_read <= 1'b0;
                   endcase
             else ack_read <= (ack_read) ? 0 : (wb_s.stb && ~wb_s.we);
