1f9e54de53f86f4d8edcb575f9d23395e3693b6f
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2018-12-14 18:24:34 +0100

    Synthesis. It works

---
+++ controleur_memoire/synthesis/global.tcl
@@ -0,0 +1,13 @@
+## Setup Technology Environement
+# Which FPGA
+setup_design -manufacturer Altera -family "Cyclone II" -part EP2C35F672C -speed 6
+# What frequency in MHz
+setup_design -frequency=400
+# Do we add IO buffers
+setup_design -addio=false
+# generate netlists in verilog and edif
+setup_design -verilog=true -edif=true
+# For Quartus new Timing ANALYSIS tool (timeQuest)
+setup_design -timequest_sdc
+# For post syn back annotated netlist (ba)
+setup_place_and_route -flow "Quartus II Modular" -command "Integrated Place and Route" -ba_format Verilog
