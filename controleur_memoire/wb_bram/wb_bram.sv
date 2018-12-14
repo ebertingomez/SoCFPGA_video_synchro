84c431279d38e0d77249a46e960e3bcb221d6da9
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2018-12-14 18:25:33 +0100

    Synthesis. It works

--- controleur_memoire/wb_bram/wb_bram.sv
+++ controleur_memoire/wb_bram/wb_bram.sv
@@ -15,9 +15,10 @@ module wb_bram #(parameter mem_adr_width = 11) (
       logic [mem_adr_width -1 : 0] address;
       logic [mem_adr_width -1 : 0] i;
 
-      assign address = wb_s.adr[mem_adr_width+1:2] + i;
 
+      assign address = wb_s.adr[mem_adr_width+1:2] + i;
       assign wb_s.ack = (wb_s.stb && wb_s.we) | ack_read;
+
 
       always_ff @(posedge wb_s.clk or posedge wb_s.rst)
       begin
@@ -26,7 +27,7 @@ module wb_bram #(parameter mem_adr_width = 11) (
                   i <= 0;
                   ack_read <= 0;
             end
-            if ( wb_s.stb && wb_s.we )
+            else if ( wb_s.stb && wb_s.we )
                   case(wb_s.sel)
                         4'b0001: memory[address][0] <= wb_s.dat_ms[7:0];
                         4'b0010: memory[address][1] <= wb_s.dat_ms[15:8];
@@ -37,32 +38,26 @@ module wb_bram #(parameter mem_adr_width = 11) (
                         4'b1111: memory[address] <= wb_s.dat_ms;
                   endcase
             else if (wb_s.stb && ~wb_s.we)
-                  case(wb_s.sel)
-                        4'b0001: wb_s.dat_sm <= {24'b0,memory[address][0]};
-                        4'b0010: wb_s.dat_sm <= {16'b0,memory[address][1],8'b0};
-                        4'b0100: wb_s.dat_sm <= {8'b0,memory[address][2],16'b0};
-                        4'b1000: wb_s.dat_sm <= {memory[address][3],24'b0};
-                        4'b0011: wb_s.dat_sm <= {16'b0,memory[address][1:0]};
-                        4'b1100: wb_s.dat_sm <= {memory[address][3:2],16'b0};
-                        4'b1111: wb_s.dat_sm <= {memory[address]};
-                  endcase
+            begin 
+                  wb_s.dat_sm <= {memory[address]};
 
-            if ( wb_s.cti != 0 && wb_s.stb && ~wb_s.we)
-                  case(wb_s.cti)
-                        3'b001: ack_read <= 1'b1;
-                        3'b010: begin
-                              ack_read <= 1'b1;
-                              i <= 1;
-                        end 
-                        3'b111: begin
-                              ack_read <= (~ack_read) ? 1'b1 : 1'b0;
-                              i <= 0;
-                        end 
-                  endcase
-            else begin 
-			ack_read <= (ack_read) ? 0 : (wb_s.stb && ~wb_s.we);
-			i <= 0;
-		end
+                  if ( wb_s.cti != 0)
+                        case(wb_s.cti)
+                              3'b001: ack_read <= 1'b1;
+                              3'b010: begin
+                                    ack_read <= 1'b1;
+                                    i <= 1;
+                              end 
+                              3'b111: begin
+                                    ack_read <= (~ack_read) ? 1'b1 : 1'b0;
+                                    i <= 0;
+                              end 
+                        endcase
+                  else begin 
+                        ack_read <= (ack_read) ? 0 : (wb_s.stb && ~wb_s.we);
+                        i <= 0;
+                  end
+            end
       end
       
 endmodule
