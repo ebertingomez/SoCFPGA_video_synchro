3177a7e981424b87bdd5ea791067ba91a3b7bc8d
Enrique <enrique.gomez@telecom-paristech.fr>
2018-12-31 02:41:07 +0100

    WIP

--- SoCFPGA/src/vga.sv
+++ SoCFPGA/src/vga.sv
@@ -30,10 +30,11 @@ begin
     else begin
         counterPixels   <= (counterPixels<HDISP+HFP+HPULSE+HBP-1) ? counterPixels+1 : 0;
 
-        if ( counterLines<VDISP+VFP+VPULSE+VBP-1 ) begin
+        if ( counterLines<VDISP+VFP+VPULSE+VBP-1 ) 
+        begin
             if (counterPixels==HDISP+HFP+HPULSE+HBP-1)
-                counterLines <= counterLines + 1;
-        end else begin
+                counterLines <= counterLines + 1; 
+        else
             counterLines <= 0;
         end
 
