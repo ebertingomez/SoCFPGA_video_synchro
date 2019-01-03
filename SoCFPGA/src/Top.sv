1e576d269e191037b4bcc685a8be5bbd433e2bca
Enrique <enrique.gomez@telecom-paristech.fr>
2019-01-03 10:41:52 +0100

    First correction to the module VGA and instantion of it on Top.sv

--- SoCFPGA/src/Top.sv
+++ SoCFPGA/src/Top.sv
@@ -63,14 +63,14 @@ assign wshb_if_stream.rty =  1'b0 ;
 // pour l'instant
 // A SUPPRIMER PLUS TARD
 //=============================
-assign wshb_if_sdram.stb  = 1'b0;
-assign wshb_if_sdram.cyc  = 1'b0;
-assign wshb_if_sdram.we   = 1'b0;
-assign wshb_if_sdram.adr  = '0  ;
-assign wshb_if_sdram.dat_ms = '0 ;
-assign wshb_if_sdram.sel = '0 ;
-assign wshb_if_sdram.cti = '0 ;
-assign wshb_if_sdram.bte = '0 ;
+// assign wshb_if_sdram.stb  = 1'b0;
+// assign wshb_if_sdram.cyc  = 1'b0;
+// assign wshb_if_sdram.we   = 1'b0;
+// assign wshb_if_sdram.adr  = '0  ;
+// assign wshb_if_sdram.dat_ms = '0 ;
+// assign wshb_if_sdram.sel = '0 ;
+// assign wshb_if_sdram.cti = '0 ;
+// assign wshb_if_sdram.bte = '0 ;
 
 //--------------------------
 //------- Code Eleves ------
@@ -120,7 +120,8 @@ end
 vga #(.HDISP(HDISP), .VDISP(VDISP) ) vga_inst(
 	.pixel_clk (pixel_clk ),
     .pixel_rst (pixel_rst ),
-    .video_ifm (video_ifm)
+    .video_ifm (video_ifm),
+    .wshb_ifm (wshb_if_sdram.master),
 );
 
 
