16688179873d12cba7e4aaea5171bebdc6623bef
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2019-01-03 22:43:32 +0100

    Squashed commit of the following:
    commit 1e576d269e191037b4bcc685a8be5bbd433e2bca
    Author: Enrique <enrique.gomez@telecom-paristech.fr>
    Date:   Thu Jan 3 10:41:52 2019 +0100
    
        First correction to the module VGA and instantion of it on Top.sv

--- SoCFPGA/src/vga.sv
+++ SoCFPGA/src/vga.sv
@@ -5,11 +5,13 @@ module vga #(parameter HDISP = 800, parameter VDISP = 480)(
     input   wire        pixel_clk,
     input   wire        pixel_rst,
     // Signals from the Interface Video
-    video_if.master     video_ifm
+    video_if.master     video_ifm,
+    // Signals from the Interface Wishbone
+    wshb_if.master      wshb_ifm
 );
 
 // Local temporal parameters for the display. 
-// Hortizaontal parameters are in pixels and vertical ones are in lines
+// Horizontal parameters are in pixels and vertical ones are in lines
 localparam HFP      =   40;
 localparam HPULSE   =   48;
 localparam HBP      =   40;
@@ -21,23 +23,31 @@ logic  [$clog2(HDISP+HFP+HPULSE+HBP)-1:0] counterPixels;
 logic  [$clog2(VDISP+VFP+VPULSE+VBP)-1:0] counterLines;
 logic   adder;
 
+// Video Interface Clock
 assign video_ifm.CLK = pixel_clk;
+
+// Wisbone Interface signals
+assign wshb_ifm.stb  = 1'b1;
+assign wshb_ifm.cyc  = 1'b1;
+assign wshb_ifm.we   = 1'b1;
+assign wshb_ifm.adr  = '0  ;
+assign wshb_ifm.dat_ms = 32'hBABECAFE ;
+assign wshb_ifm.sel = 4'b1111 ;
+assign wshb_ifm.cti = '0 ;
+assign wshb_ifm.bte = '0 ;
 
 always_ff @(posedge pixel_clk or posedge pixel_rst)
 begin
     if ( pixel_rst ) 
     begin
         // Initialisation of the values of all the signals
-        {counterPixels,counterLines} <= 0;
-        video_ifm.HS <= 1;
-        video_ifm.VS <= 1;
-        video_ifm.BLANK <= 0;
-        adder <= 0;
+        {counterPixels,counterLines,video_ifm.BLANK,adder} <= '0;
+        {video_ifm.HS,video_ifm.VS} <= 2'b11;
     end
     else begin
         // Counters evolutions
-        counterPixels   <= (counterPixels<HDISP+HFP+HPULSE+HBP-1) ? counterPixels+1 : 0;
-        counterLines    <= (counterLines<VDISP+VFP+VPULSE+VBP) ? counterLines+adder : 0;
+        counterPixels   <= (counterPixels<HDISP+HFP+HPULSE+HBP-1) ? counterPixels+1 : '0;
+        counterLines    <= (counterLines<VDISP+VFP+VPULSE+VBP) ? counterLines+adder : '0;
         // Relative adder to the line number
         adder           <= (counterPixels==HDISP+HFP+HPULSE+HBP-3) ? 1 : 0 ;
 
